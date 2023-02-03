ce47ce940000b51d4e476b472d726386da143258
Belen Sanchez <102567483+Belu-Sanchez@users.noreply.github.com>
2023-02-03 10:55:47 -0300

    update/elimine comentarios

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,11 +1,9 @@
 import axios from 'axios';
 
 const BASE_IMAGE = "https://image.tmdb.org/t/p/original";
-const MOVIE_POPULAR = "/movie/popular"; // https://api.themoviedb.org/3/movie/popular?api_key=<<api_key>>&language=en-US&page=1
+const MOVIE_POPULAR = "/movie/popular";
 const MOVIE_TOP_RATED = "/movie/top_rated";
-const SEARCH = "/search/company"; // https://api.themoviedb.org/3/search/company?api_key=<<api_key>>&page=1
-//const MOVIE_ID = `movie/${movie_id}`
-// https://api.themoviedb.org/3/movie/{movie_id}?api_key=<<api_key>>&language=en-US
+const SEARCH = "/search/company"; 
 
  const apiMovies = axios.create({
     baseURL: 'https://api.themoviedb.org/3/',
