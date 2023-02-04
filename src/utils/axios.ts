5151338211d39b604f44bdce06a6f6a8583e7ad7
Belen Sanchez <102567483+Belu-Sanchez@users.noreply.github.com>
2023-02-03 22:31:47 -0300

    update/cambios en los parametros guardados

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -3,7 +3,9 @@ import axios from 'axios';
 const BASE_IMAGE = "https://image.tmdb.org/t/p/original";
 const MOVIE_POPULAR = "/movie/popular";
 const MOVIE_TOP_RATED = "/movie/top_rated";
-const SEARCH = "/search/company"; 
+const SEARCH_MOVIE = "/search/movie"; 
+const MOVIE_UPCOMING = "/movie/upcoming"; 
+const MOVIE_LATEST = "/movie/latest"; 
 
  const apiMovies = axios.create({
     baseURL: 'https://api.themoviedb.org/3/',
@@ -13,4 +15,4 @@ const SEARCH = "/search/company";
 })
 
 
-export { apiMovies, BASE_IMAGE, MOVIE_POPULAR, MOVIE_TOP_RATED, SEARCH }
+export { apiMovies, BASE_IMAGE, MOVIE_POPULAR, MOVIE_TOP_RATED, SEARCH_MOVIE, MOVIE_UPCOMING, MOVIE_LATEST }
