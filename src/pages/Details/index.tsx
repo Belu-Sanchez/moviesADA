043ab1bf3f73d93388d0e70de8002ec1da6172db
Belen Sanchez <102567483+Belu-Sanchez@users.noreply.github.com>
2023-02-17 23:06:05 -0300

    update/guardo en un estado los datos para pintar el detalle de la movie y lo logro pintar

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -1,7 +1,38 @@
-const Details = () => {
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { BASE_IMAGE } from "../../constants";
+import { getById } from "../../services";
+type Props = {
+    title?: String,
+    overview: String,
+    poster_path: String
+
+}
+const Details = ({ }) => {
+    const { id } = useParams();
+    const [detail, setDetails] = useState<Props[]>([])
+
+    useEffect(() => {
+
+        getById(id || "").then(response => setDetails([{
+            title: response.title,
+            overview: response.overview,
+            poster_path: response.poster_path
+        }]))
+    }, [])
+
 
     return (
-        <h6 className="title">Details</h6>
+        <>
+            <h6 className="title">{detail[0].title}</h6>
+            <p>{detail[0].overview}</p>
+            <img className="col card-block"
+                src={`${BASE_IMAGE + detail[0].poster_path}`}
+                alt="First slide"
+            />
+
+
+        </>
     );
 }
 
