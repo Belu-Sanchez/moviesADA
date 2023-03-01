a06ba28f2988bc2f6b04412646b363615840478e
Belen Sanchez <102567483+Belu-Sanchez@users.noreply.github.com>
2023-03-01 07:46:59 -0300

    update/se oculta el header con la barra de navegacion

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,9 +1,27 @@
+import { Button } from "../../components/commons";
 import { SignUpForm } from "../../components/forms";
 import { withAuth } from "../../hoc";
+import { IoLogInSharp } from "react-icons/io5";
+import { useNavigate } from "react-router-dom";
+import './styles.scss'
+
 
 const SignUpPage = () => {
+
+    const navigate = useNavigate();
+
     return (
-        <SignUpForm />
+        <>
+            <div className="signup">
+                <Button
+                    type="button"
+                    variant="outline-dark btn-tabla m-2"
+                    handleClick={() => navigate('/login')}
+                ><IoLogInSharp /></Button>
+            </div>
+            <SignUpForm />
+        </>
+
     );
 }
 
