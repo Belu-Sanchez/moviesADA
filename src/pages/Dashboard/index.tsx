09cd9fb1853964f54da08f0204a32f8c763d081c
Belen Sanchez <102567483+Belu-Sanchez@users.noreply.github.com>
2023-03-02 08:32:49 -0300

    update/ preparando el catch por si hay algun problema en la api

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -10,9 +10,9 @@ const DashboardPage = () => {
 
     useEffect(() => {
 
-        getBanner().then(response => setMovies(response.splice(0, 5)))
-        getAllPopular().then(response => setPopular(response.results))
-        getAllTopRated().then(response => setTopRated(response.results))
+        getBanner().then(response => setMovies(response.splice(0, 5))).catch(response => alert("Opps! Cannot load banner, please reenter."))
+        getAllPopular().then(response => setPopular(response.results)).catch(response => alert("Oops! Can't load the most popular silver, please reenter."))
+        getAllTopRated().then(response => setTopRated(response.results)).catch(response => alert("oops! Cannot load top rated movies, please reenter."))
     }, []);
 
 
